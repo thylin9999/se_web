08ae233a55d7472e76055900bf4d58de0a10d431
drawin <andylou9527@gmail.com>
2024-03-10 17:15:16 +0800

    edit-style

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ import { createPinia } from 'pinia'
 import "./style/base.css"
 import App from './App.vue'
 import router from './router'
-import { Swipe, SwipeItem } from 'vant';
+import { Swipe, SwipeItem,Icon } from 'vant';
 import 'vant/lib/index.css';
 const app = createApp(App)
 app.use(Swipe);
 app.use(SwipeItem);
+app.use(Icon);
 app.use(createPinia())
 app.use(router)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import {onMounted, reactive} from "vue";
+import {onMounted, reactive, ref} from "vue";
 
+const swiper = ref<any>(null)
 import axios from 'axios';
-// import logo from "../assets/imgs/logo.png"
-// import logo from "../assets/imgs/logo1.webp"
-// import logo from "../assets/imgs/logo2.webp"
 import logo from "../assets/imgs/logo3.webp"
-// import logo from "../assets/imgs/logo4.webp"
-// import logo from "../assets/imgs/logo5.webp"
-// import logo from "../assets/imgs/logo6.webp"
 
 const state = reactive({
     bannerList: [],
@@ -18,8 +13,12 @@ const state = reactive({
     baseUrl: 'https://9z6wbhg0afsd.xyz/uploads/'
 })
 
+const clickArrow = function (type: any) {//
+    if (type === 'l') swiper.value.prev()
+    if (type === 'r') swiper.value.next()
+}
+
 const download = function (item: any) {
-    console.log(item)
     let type: string = ''
     const userAgent: any = navigator.userAgent.toLowerCase();
     if (/iphone|ipad|ipod/.test(userAgent)) {
@@ -35,15 +34,15 @@ const download = function (item: any) {
         window.open(item.android_download)
     } else {
         console.log('other')
+        window.open(item.ios_download)
     }
 
-    // 统计点击量  非安卓或ios 不计数
-    if (type !== 'iOS' && type !== 'Android') return
+    // 统计点击量  非安卓或ios  不计数
+    // if (type !== 'iOS' && type !== 'Android') return
     let app_type = type === "iOS" ? 2 : 1
     axios({
         method: 'post',
-        url: 'https://9z6wbhg0afsd.xyz/api/products',
-        // url: '/api/addDownload',
+        url: 'https://9z6wbhg0afsd.xyz/api/addDownload',
         data: {
             app_id: item.id,
             type: app_type,   //(1安卓,2IOS)
@@ -57,8 +56,8 @@ const getBannerList = function () {
     axios({
         method: 'post',
         url: 'https://9z6wbhg0afsd.xyz/api/products',
-        // url: '/api/products',
         data: {
+            pagesize: 1000,
             type: '1',   //产品类型 1:轮播,2:列表,3:推荐
         }
     }).then(res => {
@@ -70,8 +69,8 @@ const getBannerList = function () {
     axios({
         method: 'post',
         url: 'https://9z6wbhg0afsd.xyz/api/products',
-        // url: '/api/products',
         data: {
+            pagesize: 1000,
             type: '2',   //产品类型 1:轮播,2:列表,3:推荐
         }
     }).then(res => {
@@ -83,8 +82,8 @@ const getBannerList = function () {
     axios({
         method: 'post',
         url: 'https://9z6wbhg0afsd.xyz/api/products',
-        // url: '/api/products',
         data: {
+            pagesize: 1000,
             type: '3',   //产品类型 1:轮播,2:列表,3:推荐
         }
     }).then(res => {
@@ -135,7 +134,9 @@ onMounted(() => {
         </div>
         <div class="banner-list-box">
             <div class="list">
-                <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+                <van-icon class="arrow arrow-left" @click="clickArrow('l')" name="arrow-left"/>
+                <van-icon class="arrow arrow-right" @click="clickArrow('r')" name="arrow"/>
+                <van-swipe ref="swiper" class="my-swipe" :autoplay="3000" indicator-color="white">
                     <van-swipe-item v-for="(item,i) in state.bannerList">
                         <img class="banner" :src="item.product_img" @click="download(item)" alt="">
                     </van-swipe-item>
@@ -143,10 +144,10 @@ onMounted(() => {
             </div>
         </div>
         <div class="down-list-box">
-            <div class="d-list" v-for="(item,i) in state.picList" :key="i">
+            <div class="d-list" v-for="(item,i) in state.picList" :key="i" @click="download(item)">
                 <img :src="item.product_img" alt="">
                 <div class="txt txt-name">{{ item.product_name }}</div>
-                <div class="txt txt-download" @click="download(item)">↓下载</div>
+                <div class="txt txt-download">↓下载</div>
             </div>
             <div v-for="i in 5 - 22%5" class="d-list" :key="i">
             </div>
@@ -185,7 +186,6 @@ onMounted(() => {
         justify-content: space-between;
 
         .left {
-            //margin-left: 10px;
             border-radius: 5px;
             height: 60px;
             overflow: hidden;
@@ -194,8 +194,6 @@ onMounted(() => {
             img {
                 border-radius: 4px;
                 height: 40px;
-                //height: 30px;
-                //width: 20px;
                 margin: 10px 5px;
                 border: 1px solid #F01D27;
             }
@@ -219,8 +217,38 @@ onMounted(() => {
         width: 100%;
         height: auto;
 
+        .list {
+            position: relative;
+
+            .arrow {
+                @media (max-width: 460px) {
+                    display: none;
+                }
+                width: 100px;
+                height: 100px;
+                cursor: pointer;
+                top: 0;
+                bottom: 0;
+                margin: auto;
+                position: absolute;
+                color: #fff;
+                opacity: .4;
+                font-size: 80px;
+                z-index: 1000;
+            }
+
+            .arrow-left {
+                left: -10px;
+            }
+
+            .arrow-right {
+                right: -30px;
+            }
+        }
+
         .my-swipe {
             border-radius: 6px;
+
         }
 
         .banner {
@@ -228,6 +256,8 @@ onMounted(() => {
             width: 98%;
             margin: auto;
             border-radius: 6px;
+            user-drag: none;
+            -webkit-user-drag: none;
         }
     }
 
@@ -239,6 +269,7 @@ onMounted(() => {
         justify-content: space-around;
 
         .d-list {
+            cursor: pointer;
             width: 18%;
             margin: 10px 0;
             display: flex;
@@ -261,18 +292,25 @@ onMounted(() => {
 
             .txt-name {
                 color: #666;
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
             }
 
             .txt-download {
                 cursor: pointer;
                 color: #ff495c;
                 border-radius: 27px;
-                width: 70px;
+                //width: 70px;
+                width: 100%;
                 height: 27px;
                 border: 2px solid #ff495c;
                 margin: auto;
                 font-size: 14px;
                 line-height: 26px;
+                @media (min-width: 460px) {
+                    width: 70px;
+                }
             }
         }
 
